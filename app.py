b356f3532ba29d41563d197c70134170a4eb066d
unknown <alexanisandr@gmail.com>
2023-03-14 18:46:45 +0200

    alal

--- app.py
+++ app.py
@@ -6,13 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/add')
-def add():
-    return render_template('add.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
