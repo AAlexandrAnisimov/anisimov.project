27d41b815ddc08d7f054038c4f83db4946bfb1b8
unknown <alexanisandr@gmail.com>
2023-03-14 20:28:57 +0200

    alxk

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+server = Flask(__name__)
 
-@app.route('/')
+@server.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/about')
+@server.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/add')
+@server.route('/add')
 def add():
     return render_template('add.html')
 
+@server.route('/post')
+def post():
+    return render_template('post.html')
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
