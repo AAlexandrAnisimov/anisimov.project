6999242a7677ca50079eed8d18d96c0642113cb6
unknown <alexanisandr@gmail.com>
2023-03-14 19:07:34 +0200

    akakak

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
