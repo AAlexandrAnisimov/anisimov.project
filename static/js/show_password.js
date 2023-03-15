c2319a24495e15cf33c51215e647f0ff39309238
unknown <alexanisandr@gmail.com>
2023-03-15 17:47:16 +0200

    qksk

--- static/js/show_password.js
+++
@@ -1,6 +0,0 @@
-function ShowPassword(obj) {
-    var x = document.getElementById("myInput");
-    x.type = (x.type == "password") ? "text" : "password";
-
-    obj.classList.toggle("fa-eye-slash");
-}
