41dc187e05803b563b34bac57580670285746ab2
unknown <alexanisandr@gmail.com>
2023-03-15 21:33:00 +0200

    qweasd

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def course(course_id):
     #cursor.execute("select * from courses where courses.fk_user_id = 666 and courses.course_id = 1")
     cursor.execute(f"SELECT get_course_info()")
     result = cursor.fetchone()
-    # (course_id, user_id, title, subtitle, day_posted, content) = result[0][1:-1].split(',')
+    (course_id, user_id, title, subtitle, day_posted, content) = result.split(',')
     # (course_id, user_id, title, subtitle, day_posted, content) = cursor.fetchone().split(',')
     connection.close()
     
