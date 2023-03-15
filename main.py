3d41c6a28650299c91298e88697461f1d091ae98
unknown <alexanisandr@gmail.com>
2023-03-15 21:25:42 +0200

    qq

--- main.py
+++ main.py
@@ -55,7 +55,10 @@ def course(course_id):
     connection.autocommit = True
 
     cursor = connection.cursor()
-    cursor.execute("select * from courses where courses.fk_user_id = 666 and courses.course_id = 1")
+    #cursor.execute("select * from courses where courses.fk_user_id = 666 and courses.course_id = 1")
+    cursor.execute(f"SELECT get_course_info({(666, 1)})")
+    result = cursor.fetchall()[0]
+    # (course_id, user_id, title, subtitle, day_posted, content) = result[0][1:-1].split(',')
     # (course_id, user_id, title, subtitle, day_posted, content) = cursor.fetchone().split(',')
     connection.close()
     
