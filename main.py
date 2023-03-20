60d30e93b26cb5ea87e411138c15d47849a3e35e
unknown <anisoleksandr@gmail.com>
2023-03-20 19:10:57 +0200

    alql

--- main.py
+++ main.py
@@ -150,12 +150,13 @@ def index():
         
         courses_lst = []
         for course_id, t_id, title, subtitle, content, day_posted in result:
+            user = get_user_by_id(t_id)[0]
             course = {
                 "id": course_id,
                 "title": title,
                 "subtitle": subtitle,
                 "content": content,
-                "posted_by": "Zeleboba",
+                "posted_by": user['login'],
                 "day_posted": day_posted
             }
             courses_lst.append(course)
